520484f36e25aa743e575b2f40a2abd77258a3c4
Saif Shaikh <145893524+ZenonWrites@users.noreply.github.com>
2025-10-11 17:09:29 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-// frontend/src/App.js
+
 import React, { useState, useEffect } from 'react';
 import ProjectList from './components/ProjectList';
 import Layout from './components/Layout'; // Import the Layout component
@@ -7,16 +7,16 @@ function App() {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    // This fetching logic stays the same
+   
     fetch('http://127.0.0.1:8000/api/projects/')
       .then(response => response.json())
       .then(data => setProjects(data));
   }, []);
 
   return (
-    // Wrap everything in the Layout component
+    
     <Layout> 
-      {/* The ProjectList is now a "child" of the Layout */}
+      {/* adding this component into layout */}
       <ProjectList projects={projects} />
     </Layout>
   );
